02e9ed0a86d87e60fd610186f9fef1c8c23fdbb8
simos <sim.apergis@gmail.com>
2023-01-23 23:55:29 +0200

    Added posted days ago

--- src/main/java/com/example/movierama/movie/MovieDTO.java
+++ src/main/java/com/example/movierama/movie/MovieDTO.java
@@ -5,6 +5,8 @@ import lombok.*;
 
 import java.time.LocalDate;
 
+import static java.time.temporal.ChronoUnit.DAYS;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -25,6 +27,8 @@ public class MovieDTO {
     Long userId;
     @JsonProperty("postedOn")
     LocalDate publicationDate;
+    @JsonProperty("postedWhen")
+    String postedWhen;
     @JsonProperty("likes")
     int likes;
     @JsonProperty("hates")
@@ -35,4 +39,19 @@ public class MovieDTO {
     boolean hated;
     @JsonProperty("owned")
     boolean owned;
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+        setPostedWhen();
+    }
+
+    public void setPostedWhen() {
+        final long postedDaysAgo = DAYS.between(this.publicationDate, LocalDate.now());
+
+        switch ((int) postedDaysAgo) {
+            case 0 -> this.postedWhen = "Today";
+            case 1 -> this.postedWhen = "1 day ago";
+            default -> this.postedWhen = postedDaysAgo + " days ago";
+        }
+    }
 }
