9086ace45a08133607f1c274ce41b36a6fd6d224
simos <sim.apergis@gmail.com>
2023-01-27 00:47:32 +0200

    Fixed bug with posted time in UI

--- src/main/java/com/example/movierama/movie/MovieDTO.java
+++ src/main/java/com/example/movierama/movie/MovieDTO.java
@@ -47,7 +47,7 @@ public class MovieDTO {
 
     public void setPostedWhen() {
         final long postedDaysAgo = ChronoUnit.DAYS.between(this.publicationDate, LocalDateTime.now());
-        long minutesAgo = 0;
+        long minutesAgo = -1;
         long hoursAgo = 0;
 
         if (postedDaysAgo == 0)
@@ -56,10 +56,15 @@ public class MovieDTO {
         if (hoursAgo < 1)
             minutesAgo = ChronoUnit.MINUTES.between(this.publicationDate, LocalDateTime.now());
 
-        switch ((int) postedDaysAgo) {
-            case 0 -> this.postedWhen = (minutesAgo >= 0) ? minutesAgo + " minutes ago" : hoursAgo + " hours ago";
-            case 1 -> this.postedWhen = "1 day ago";
-            default -> this.postedWhen = postedDaysAgo + " days ago";
-        }
+        if (postedDaysAgo == 1)
+            this.postedWhen = "1 day ago";
+        else if (postedDaysAgo > 0)
+            this.postedWhen = postedDaysAgo + " days ago";
+        else if (minutesAgo > 0)
+            this.postedWhen = minutesAgo + " minutes ago";
+        else if (hoursAgo > 0)
+            this.postedWhen = hoursAgo + " hours ago";
+        else
+            this.postedWhen = "A few seconds ago";
     }
 }
